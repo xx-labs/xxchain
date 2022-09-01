6b852cd4f2b6da65946087dc94fc025679417de4
Bernardo Cardoso <bernardo@xx.network>
2022-09-01 21:51:46 +0000

    Fix try runtime compilation

--- runtime/xxnetwork/src/lib.rs
+++ runtime/xxnetwork/src/lib.rs
@@ -1384,8 +1384,21 @@ impl_runtime_apis! {
 			(weight, BlockWeights::get().max_block)
 		}
 
-		fn execute_block_no_check(block: Block) -> frame_support::weights::Weight {
-			Executive::execute_block_no_check(block)
+		fn execute_block(
+			block: Block,
+			state_root_check: bool,
+			select: frame_try_runtime::TryStateSelect
+		) -> Weight {
+			log::info!(
+				target: "runtime::xxnetwork",
+				"try-runtime: executing block {:?} / root checks: {:?} / try-state-select: {:?}",
+				block.header.hash(),
+				state_root_check,
+				select,
+			);
+			// NOTE: intentional unwrap: we don't want to propagate the error backwards, and want to
+			// have a backtrace here.
+			Executive::try_execute_block(block, state_root_check, select).unwrap()
 		}
 	}
 
