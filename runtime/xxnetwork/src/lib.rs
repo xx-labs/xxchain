c80113d86f817a0a30a9733c7f2ce0862bf53b48
Bernardo Cardoso <bernardo@xx.network>
2023-02-25 02:22:28 +0000

    Set transaction version back to 1

--- runtime/xxnetwork/src/lib.rs
+++ runtime/xxnetwork/src/lib.rs
@@ -114,7 +114,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 206,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 2,
+	transaction_version: 1,
 	state_version: 0,
 };
 
